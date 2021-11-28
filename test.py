6ba858a9ea2b460760df12101c73831e35a2b859
Francisco Jesús Rubio Reales <fj.rubio@dosfarma.com>
2021-11-28 23:12:00 +0100

    Aded operations

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import pprint
 import sys
 
 from google.cloud import storage
@@ -37,8 +38,9 @@ def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
 
 
 def do_help(bucket_name, *args, **kwargs):
-    pass
-
+    commands = [g for g in globals().keys() if g.startswith('do_')]
+    commands = list(map(lambda x: x[3:], commands))
+    pprint.pprint(commands)
 
 def do_ls(bucket_name, *args, **kwargs):
     print(bucket_name)
@@ -47,23 +49,36 @@ def do_ls(bucket_name, *args, **kwargs):
     # list_blobs_with_prefix(bucket_name, *args)
 
 
-def do_upload(bucket_name, *args, **kwargs):
-    source_name, dest_name = args
-
-    # upload_blob(bucket_name, source_name, dest_name)
-    pass
-
-
-def do_download(bucket_name, *args, **kwargs):
-    pass
+def do_upload(bucket_name, source_name, dest_name, *args, **kwargs):
+    """Uploads {source_name} file to the bucket {bucket_name} with name {dest_name}"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(dest_name)
+    blob.upload_from_filename(source_name)
 
 
-def do_rename(bucket_name, *args, **kwargs):
-    pass
+def do_download(bucket_name, source_name, dest_name, *args, **kwargs):
+    """donwloads {source_name} blob from {bucket_name} to {dest_name}"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_name)
+    blob.download_to_filename(dest_name)
 
 
-def do_delete(bucket_name, *args, **kwargs):
-    pass
+def do_rename(bucket_name, source_name, dest_name, *args, **kwargs):
+    """Renames blob {source_name} to {dest_name}"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_name)
+    new_blob = bucket.rename_blob(blob. dest_name)
+
+
+def do_delete(bucket_name, blob_name, *args, **kwargs):
+    """Deletes a blob {blob_name}"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.delete()
 
 
 if __name__ == "__main__":
@@ -76,9 +91,12 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         bucket_name = sys.argv[2]
     params = tuple(sys.argv[3:])
+    args = tuple(a for a in params if '=' not in a)
+    kwargs = dict(tuple(a.split('=')) for a in params if '=' in a)
 
-    print(f'{cmd=}\n{bucket_name=}\n{params=}\n')
 
-    do_fn = locals()[f'do_{cmd}']
-    do_fn(bucket_name, params)
+    print(f'{cmd=}\n{bucket_name=}\n{params=}\n{args=}\n{kwargs=}')
+
+    run_fn = locals()[f'do_{cmd}']
+    run_fn(bucket_name, *args, **kwargs)
 
